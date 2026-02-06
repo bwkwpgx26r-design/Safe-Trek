1eb676b055d79084798a7c6df08e2bb88bb338a3
bwkwpgx26r-design <timonj@fam-fehns.de>
2026-02-06 10:15:10 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = "safetrek-beta-v1";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).then((resp) => {
+      const copy = resp.clone();
+      caches.open(CACHE).then((c) => c.put(req, copy));
+      return resp;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
